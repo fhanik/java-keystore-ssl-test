b5390ca56ee90d6c928718b5164fc936dee37268
Filip Hanik <fhanik@pivotal.io>
2015-04-08 14:16:27 -0600

    Use the correct directory as a reference

--- ssl-certificate/run-commands.sh
+++ ssl-certificate/run-commands.sh
@@ -3,6 +3,7 @@
 CUR_DIR=`pwd`
 
 BASE_DIR=`dirname "$(readlink -f "$0")"`
+export BASE_DIR
 echo $BASE_DIR
 
 set -x -e
@@ -19,7 +20,7 @@ openssl genrsa -passout pass:password -des3 -out webserverkey.pem 2048
 
 #generate the *.127.0.0.1.xip.io cert
 openssl req -passin pass:password -new -key webserverkey.pem -out webservercert.csr -days 3650 -config ../config.txt -subj "/C=US/ST=Colorado/L=Castle Rock/O=Filips WebServer Level 1/OU=IT Department/CN=*.127.0.0.1.xip.io"
-openssl ca -batch -passin pass:password -in webservercert.csr -out webservercert.pem -config /root/ca/config.txt
+openssl ca -batch -passin pass:password -in webservercert.csr -out webservercert.pem -config $BASE_DIR/config.txt
 
 openssl rsa -passin pass:password -in webserverkey.pem -out webserverkey_nopasswd.pem
 
